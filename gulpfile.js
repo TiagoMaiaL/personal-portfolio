34cb4a1022fce10f0234ad51e08c514353ee3c21
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-11-04 14:05:28 -0200

    Adding task for styles and js.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,30 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   rename = require('gulp-rename'),
   cleanCss = require('gulp-clean-css'),
-  prefixer = require('gulp-auto-prefixer'),
+  prefixer = require('gulp-autoprefixer'),
   uglify = require('gulp-uglify'),
   resize = require('gulp-image-resize');
 
-gulp.task('styles', function() {});
+// TODO: include source maps.
 
-gulp.task('scripts', function() {});
+gulp.task('styles', function() {
+  gulp.src('resources/styles/*.css')
+    .pipe(concat())
+    .pipe(prefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCss())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('scripts', function() {
+  gulp.src('js/*.js')
+    .pipe(concat())
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('build/'));
+});
 
 gulp.task('images', function() {});
