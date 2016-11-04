3bb22d771c6c63f4699090860101c1a5d9a91706
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-11-04 13:44:40 -0200

    initial gulpfile code.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  cleanCss = require('gulp-clean-css'),
+  prefixer = require('gulp-auto-prefixer'),
+  uglify = require('gulp-uglify'),
+  resize = require('gulp-image-resize');
+
+gulp.task('styles', function() {});
+
+gulp.task('scripts', function() {});
+
+gulp.task('images', function() {});
