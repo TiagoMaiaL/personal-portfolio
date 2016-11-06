3d8797322793319fb72250f19446566b0d5e5acf
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-11-06 12:18:28 -0200

    Adding browser-sync tasks with gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,45 @@ var gulp      = require('gulp'),
   prefixer    = require('gulp-autoprefixer'),
   uglify      = require('gulp-uglify'),
   responsive  = require('gulp-responsive'),
-  imagemin    = require('gulp-imagemin');
+  imagemin    = require('gulp-imagemin'),
+  browserSync = require('browser-sync').create();
+
+var templatesPath = './**/*.html'; 
+  stylesPath      = 'resources/styles/*.css',
+  jsPath          = 'js/*.js',
+  imagesPath      = 'resources/images/*.{jpeg,gif,png}',
+  imagesBuildPath = 'build/resources/images';
+
 
 // TODO: include source maps.
 
 gulp.task('styles', function() {
-  gulp.src('resources/styles/*.css')
-    .pipe(concat())
+  gulp.src(stylesPath)
+    .pipe(concat('styles.all.css'))
     .pipe(prefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cleanCss())
     .pipe(rename('main.min.css'))
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('build/styles'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function() {
-  gulp.src('js/*.js')
-    .pipe(concat())
+  gulp.src(jsPath)
+    .pipe(concat('js.all.css'))
     .pipe(uglify())
     .pipe(rename('main.min.js'))
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('scripts-watch', ['scripts'], function() {
+  browserSync.reload();
+  done();
 });
 
 gulp.task('images', function() {
-  var imagesPath = 'resources/images/*.{jpeg,gif,png}';
-  var buildPath = 'build/resources/images';
-
   gulp.src(imagesPath)
     .pipe(responsive([
       {
@@ -58,7 +69,19 @@ gulp.task('images', function() {
         withMetaData: false
       }))
     .pipe(imagemin())
-    .pipe(gulp.src(buildPath));
+    .pipe(gulp.src(imagesBuildPath));
 });
 
-gulp.task('images', function() {});
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('serve', ['browser-sync', 'styles'], function() {
+  gulp.watch(stylesPath, ['styles']);
+  gulp.watch(jsPath, ['scripts-watch']);
+  gulp.watch(templatesPath).on('change', browserSync.reload);
+});
