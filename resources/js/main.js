00016c3429b50a8ab9d01ef64a18ba1424d5c258
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-11-19 00:50:35 -0200

    Adding code to initialize masonry plugin.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,13 @@
-$('.masonry').masonry({
+$(document).ready(function() {
+
+  /**
+   * Initializing masonry grid.
+   */
+  $('.masonry').masonry({
+    itemSelector: '.masonry-item',
+    columnWidth: '.masonry-sizer',
+    percentPosition: true
+  });
 
 });
+
