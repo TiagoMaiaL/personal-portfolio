c9fc87ab090c6995bf8b5896cf41dfdd91eca482
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-11-20 19:41:11 -0200

    Adding task to build fonts and concatenate all related vendors into one file.

--- gulpfile.js
+++ gulpfile.js
@@ -8,61 +8,21 @@ var gulp      = require('gulp'),
   bower       = require('gulp-main-bower-files'),
   filter      = require('gulp-filter'),
   flatten     = require('gulp-flatten'),
+  replace     = require('gulp-replace'),
   browserSync = require('browser-sync').create();
 
 // TODO: include source maps.
-// TODO: include one main file for js and vendors
-// TODO: include one main file for css and vendors
+// TODO: Use promises instead of timeouts.
 
 var templatesPath = './**/*.html';
   stylesPath      = './resources/styles/*.css',
+  fontelloPath    = './resources/fonts/fontello/css/fontello.css',
   jsPath          = './resources/js/*.js',
   imagesPath      = './resources/images/*.{jpg,gif,png}',
   imagesBuildPath = './build/resources/images',
   bowerPath       = './bower_components/**/*';
 
-gulp.task('styles', function() {
-  gulp.src(stylesPath)
-    .pipe(concat('styles.all.css'))
-    .pipe(prefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .on('error', console.log.bind(console))
-    .pipe(cleanCss())
-    .pipe(rename('main.min.css'))
-    .pipe(gulp.dest('build/styles'))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('scripts', function() {
-  gulp.src(jsPath)
-    .pipe(concat('js.all.css'))
-    .pipe(uglify())
-    .pipe(rename('main.min.js'))
-    .pipe(gulp.dest('build/js'));
-});
-
-gulp.task('scripts-watch', ['scripts'], function() {
-  browserSync.reload();
-  done();
-});
-
-gulp.task('fonts', function() {
-  var bowerFontsGlob = "bower_components/**/fonts/*.{eot,svg,ttf,woff,woff2}";
-  var vendorFontsGlob = 'resources/fonts/**/*.{eot,svg,ttf,woff,woff2}';
-
-  gulp.src(bowerFontsGlob)
-    .pipe(flatten())
-    .pipe(gulp.dest('build/fonts'));
-
-  gulp.src(vendorFontsGlob)
-    .pipe(flatten())
-    .pipe(gulp.dest('build/fonts'));
-});
-
 gulp.task('images', function() {
-
   var getConfigurations = function(options) {
     return options.map(function(option) {
       return {
@@ -104,6 +64,19 @@ gulp.task('images', function() {
     .pipe(gulp.dest(imagesBuildPath));
 });
 
+gulp.task('scripts', function() {
+  gulp.src(jsPath)
+    .pipe(concat('js.all.css'))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('scripts-watch', ['scripts'], function() {
+  browserSync.reload();
+  done();
+});
+
 gulp.task('bower-scripts', function() {
   var jsGlob    = '**/*.js',
       jsFilter  = filter([jsGlob], {restore: true});
@@ -135,6 +108,20 @@ gulp.task('bower-scripts', function() {
     .pipe(gulp.dest('build/js/vendor'))
 });
 
+gulp.task('styles', function() {
+  return gulp.src(stylesPath)
+    .pipe(concat('styles.all.css'))
+    .pipe(prefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .on('error', console.log.bind(console))
+    .pipe(cleanCss())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('build/styles'))
+    .pipe(browserSync.stream());
+});
+
 gulp.task('bower-styles', function() {
   var cssGlob   = '**/*.css',
       cssFilter = filter([cssGlob], {restore: true});
@@ -147,10 +134,41 @@ gulp.task('bower-styles', function() {
     .on('error', console.log.bind(console))
     .pipe(rename('vendor.min.css'))
     .pipe(cssFilter.restore)
-    .pipe(gulp.dest('build/styles/vendor'));
+    .pipe(gulp.dest('build/styles'));
+
+  setTimeout(function() {
+    gulp.src([fontelloPath, 'build/styles/vendor.min.css'])
+      .pipe(concat('vendor.css'))
+      .pipe(cleanCss())
+      .on('error', console.log.bind(console))
+      .pipe(rename('vendor.min.css'))
+      .pipe(gulp.dest('build/styles'));
+  }, 2000);
 });
 
-gulp.task('bower-watch', ['bower-scripts', 'bower-styles'], function(done) {
+gulp.task('styles-all', ['styles', 'bower-styles', 'fonts'], function() {
+  setTimeout(function() {
+    gulp.src('build/styles/vendor.min.css')
+      .pipe(replace('fonts/', '../fonts/'))
+      .pipe(replace('../font/', '../fonts/'))
+      .pipe(gulp.dest('build/styles'));
+  }, 2500);
+});
+
+gulp.task('fonts', function() {
+  var bowerFontsGlob  = 'bower_components/**/fonts/*.{eot,svg,ttf,woff,woff2}',
+      vendorFontsGlob = 'resources/fonts/**/*.{eot,svg,ttf,woff,woff2}';
+
+  gulp.src(bowerFontsGlob)
+    .pipe(flatten())
+    .pipe(gulp.dest('build/fonts'));
+
+  gulp.src(vendorFontsGlob)
+    .pipe(flatten())
+    .pipe(gulp.dest('build/fonts'));
+});
+
+gulp.task('bower-watch', ['bower-scripts', 'styles-all'], function(done) {
   browserSync.reload();
   done();
 });
