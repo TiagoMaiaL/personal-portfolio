23f4846eb1f4c241a9bd8d9e0644614888cd4859
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-11-21 01:52:49 -0200

    Adding code for handling click events.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,26 @@
-$(document).ready(function() {
+(function() {
+  $(document).ready(function() {
 
-  /**
-   * Initializing masonry grid.
-   */
-  $('.masonry').masonry({
-    itemSelector: '.masonry-item',
-    columnWidth: '.masonry-sizer',
-    percentPosition: true
+    /**
+     * Initializing masonry grid.
+     */
+    $('.masonry').masonry({
+      itemSelector: '.masonry-item',
+      columnWidth: '.masonry-sizer',
+      percentPosition: true
+    });
+
+    /**
+     * Handler for clicks on the project divs.
+     */
+    $('.project').click(function(event) {
+      if ($(window).width >= 900)
+        return;
+
+      $('.project').removeClass('active');
+      $(this).toggleClass('active');
+    });
+
   });
-
-});
+})()
 
